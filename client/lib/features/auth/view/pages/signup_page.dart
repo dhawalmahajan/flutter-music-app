ce0ff1cb825058ae6d2c17893644d60272907f8f
Dhawal Mahajan <dhawalmahajan@users.noreply.github.com>
2025-11-21 12:42:22 +0530

    add button on sign up page
    Add gradient button on sign up page

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,4 +1,6 @@
+import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/core/widgets/custom_field.dart';
+import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -40,6 +42,26 @@ class _SignUpPageState extends State<SignUpPage> {
                 isObscureText: true,
               ),
               const SizedBox(height: 20),
+              AuthGradientButton(buttonText: 'Sign up', onTap: () async {}),
+              // const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {},
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Already have an account? ',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: const [
+                      TextSpan(
+                        text: 'Sign In',
+                        style: TextStyle(
+                          color: Pallete.gradient2,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
