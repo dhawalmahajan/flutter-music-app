802ab8fe4fb2cbff08a970d69dbcaea29e35f015
Dhawal Mahajan <dhawalmahajan@users.noreply.github.com>
2025-12-04 17:45:08 +0530

    add  networking lib,remote repo
    add user model

---
+++ client/lib/features/auth/repositories/auth_remote_repository.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:client/core/failure/failure.dart';
+import 'package:client/features/auth/model/user_model.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:http/http.dart' as http;
+import 'package:client/core/network/base_url.dart';
+
+class AuthRemoteRepository {
+  Future<Either<AppFailure, UserModel>> signup({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/auth/signup'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'name': name, 'email': email, 'password': password}),
+      );
+      final user = jsonDecode(response.body) as Map<String, dynamic>;
+      if (response.statusCode != 201) {
+        return Left(AppFailure(user['detail']));
+      }
+
+      return Right(UserModel.fromMap(user));
+    } catch (e) {
+      print('Signup failed: $e');
+      return Left(AppFailure(e.toString()));
+    }
+  }
+
+  Future<void> login({required String email, required String password}) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/auth/login'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'email': email, 'password': password}),
+      );
+      print(response.body);
+    } catch (e) {
+      print('Signup failed: $e');
+    }
+  }
+}
