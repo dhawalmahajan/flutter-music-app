96e5a4db39d9831aebf5f0c2fcb725d34566da9e
Dhawal Mahajan <dhawalmahajan@users.noreply.github.com>
2025-12-13 10:22:41 +0530

    add file picker
    add helper to pick image and audio,
    add file picker lib, update info.plist

--- client/lib/core/utils.dart
+++ client/lib/core/utils.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String content) {
@@ -6,4 +9,37 @@ void showSnackBar(BuildContext context, String content) {
     ..showSnackBar(
       SnackBar(content: Text(content), backgroundColor: Colors.red),
     );
+}
+
+Future<File?> pickImage() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+    );
+
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+Future<File?> pickAudio() async {
+  // Function to pick audio file
+  try {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+    );
+    if (result != null && result.files.isNotEmpty) {
+      final file = File(result.files.first.path ?? '');
+      return file;
+      // Use the selected audio file
+    }
+    return null;
+  } catch (e) {
+    // Handle any errors that occur during file picking
+    return null;
+  }
 }
--- client/lib/features/home/view/pages/upload_song_page.dart
+++ client/lib/features/home/view/pages/upload_song_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/utils.dart';
 import 'package:client/core/widgets/custom_field.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
@@ -16,6 +19,35 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
   final songNameController = TextEditingController();
   final artistController = TextEditingController();
   Color selectedColor = Pallete.cardColor;
+  File? selectedImage;
+  File? selectedAudio;
+  final formKey = GlobalKey<FormState>();
+
+  void selectAudio() async {
+    final pickedAudio = await pickAudio();
+    if (pickedAudio != null) {
+      setState(() {
+        selectedAudio = pickedAudio;
+      });
+    }
+  }
+
+  void selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        selectedImage = pickedImage;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    songNameController.dispose();
+    artistController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,37 +66,52 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              DottedBorder(
-                options: RectDottedBorderOptions(
-                  color: Pallete.borderColor,
-                  dashPattern: const [10, 4],
-                  padding: const EdgeInsets.all(20),
-                  strokeCap: StrokeCap.round,
-                ),
-                child: SizedBox(
-                  height: 150,
-                  width: double.infinity,
-                  child: Column(
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
+              GestureDetector(
+                onTap: selectImage,
+                child: selectedImage != null
+                    ? SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            selectedImage!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    : DottedBorder(
+                        options: RectDottedBorderOptions(
+                          color: Pallete.borderColor,
+                          dashPattern: const [10, 4],
+                          padding: const EdgeInsets.all(20),
+                          strokeCap: StrokeCap.round,
+                        ),
+                        child: SizedBox(
+                          height: 150,
+                          width: double.infinity,
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(height: 15),
+                              Text(
+                                'Select thumbnail File to Upload',
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      SizedBox(height: 15),
-                      Text(
-                        'Select thumbnail File to Upload',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
-                  ),
-                ),
               ),
               const SizedBox(height: 40),
               CustomField(
                 hintText: 'Pick Song',
                 controller: null,
                 readOnly: true,
-                onTap: () {},
+                onTap: selectAudio,
               ),
               const SizedBox(height: 40),
               CustomField(
