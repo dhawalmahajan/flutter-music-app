72602b0e4c3df843b168d0999a2f95075132aaba
Dhawal Mahajan <dhawalmahajan@users.noreply.github.com>
2025-12-19 14:26:32 +0530

    change pubspec file and add bottom navigation bar

--- client/lib/features/home/view/pages/home_page.dart
+++ client/lib/features/home/view/pages/home_page.dart
@@ -1,14 +1,52 @@
 import 'package:client/core/providers/current_user_notifier.dart';
+import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  @override
+  Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserProvider);
+    int selectedIndex = 0;
     print(currentUser);
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              selectedIndex == 0
+                  ? 'assets/images/home_filled.png'
+                  : 'assets/images/home_unfilled.png',
+              color: selectedIndex == 0
+                  ? Pallete.whiteColor
+                  : Pallete.inactiveBottomBarItemColor,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/library.png',
+              color: selectedIndex == 1
+                  ? Pallete.whiteColor
+                  : Pallete.inactiveBottomBarItemColor,
+            ),
+            label: 'Library',
+          ),
+        ],
+      ),
+    );
   }
 }
